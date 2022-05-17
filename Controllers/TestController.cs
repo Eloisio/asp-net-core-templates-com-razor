e5663a5f4f400922479b53c3a3f291cb610e8272
Cleyson Lima <cleysonph@gmail.com>
2022-05-17 17:28:34 -0300

    03:04 - exibindo informações vindas do controller

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using TWTodoList.Models;
+
 namespace TWTodoList.Controllers;
 
 public class TestController : Controller
 {
     public IActionResult Index()
+    {
+        var todo = new Todo("Finalizar o curso de Razor da TreinaWeb", DateTime.Now);
+        ViewBag.Name = "Cleyson";
+        ViewBag.Todo = todo;
+        TempData["name"] = "Cleyson";
+        return View();
+    }
+
+    public IActionResult AnotherTest()
     {
         return View();
     }
