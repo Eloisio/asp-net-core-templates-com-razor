fcb2d9607cca08dc37a8a6616dc8a2993eeeb56c
Cleyson Lima <cleysonph@gmail.com>
2022-05-18 18:57:10 -0300

    06:11 - principais tag helpers

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -31,6 +31,7 @@ public class TestController : Controller
         return View(client);
     }
 
+    [Route("/Teste/Clients", Name = "ClientList")]
     public IActionResult ClientList()
     {
         var clients = new List<Client>();
@@ -38,5 +39,17 @@ public class TestController : Controller
         clients.Add(new Client { Name = "Juan Pablo", Email = "jua@mail.com", Purchases = 10, Type = 1 });
         clients.Add(new Client { Name = "Sara Cristine", Email = "sara@mail.com", Purchases = 10, Type = 1 });
         return View(clients);
+    }
+
+    public IActionResult TagHelpers()
+    {
+        var client = new Client
+        {
+            Name = "Cleyson Lima",
+            Email = "cleyson@mail.com",
+            Purchases = 110,
+            Type = 5,
+        };
+        return View(client);
     }
 }
